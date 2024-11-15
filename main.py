4d5e3993d82cdbb2f727ec9ca12cca21ae9ad719
Tussa Domingos <119713083+BaliNwen@users.noreply.github.com>
2024-11-15 13:52:18 -0500

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+
+
 game.splash("Welcome to the Stock Game Investor! ")
 pause(500)
 game.splash("What would you like to invest in?")
 pause(500)
-game.ask_for_string("Here are your options! NASDAQ and S&P500", 6)
+game.ask_for_string("Here are your options! NASDAQ and S&P", 6)
 
-
+if "S&P": True 
+game.splash("you've chosen the S&P!")
+pause(500)
+game.splash("This is its current Value: ")
--- main.ts
+++ main.ts
@@ -2,4 +2,11 @@ game.splash("Welcome to the Stock Game Investor! ")
 pause(500)
 game.splash("What would you like to invest in?")
 pause(500)
-game.askForString("Here are your options! NASDAQ and S&P500", 6)
+game.askForString("Here are your options! NASDAQ and S&P", 6)
+if ("S&P") {
+    true
+}
+
+game.splash("you've chosen the S&P!")
+pause(500)
+game.splash("This is its current Value: ")
