507eb58eb21cdb5870a7c9b3979f171a98f15f62
Tussa Domingos <119713083+BaliNwen@users.noreply.github.com>
2024-11-17 23:20:02 -0500

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-
-
+NASDAQ = 0
+SnP = 0
 game.splash("Welcome to the Stock Game Investor! ")
 pause(500)
 game.splash("What would you like to invest in?")
 pause(500)
-game.ask_for_string("Here are your options! NASDAQ and S&P", 6)
-
-if "S&P": True 
+game.ask_for_string("Here are your options! NASDAQ and SnP", 6)
+# if S&P true then splash their choice then ask how much they want to invest and their current wallet amount
+if "SnP":
+    True
+    SnP = randint(10000, 1000000)
 game.splash("you've chosen the S&P!")
 pause(500)
-game.splash("This is its current Value: ")
+game.splash("This is its current Value: " + "$" + str(SnP))
+# same stuff just for Nas
+if "NASDAQ":
+    True
+    NASDAQ = randint(10000, 1000000)
+game.splash("you've chosen NASDAQ")
+pause(500)
+game.splash("This is its current Value: " + "$" + str(NASDAQ))
+
+def investment(int):
+    game.ask_for_number("How much would you like to invest?")
+    return investment
+    
--- main.ts
+++ main.ts
@@ -1,12 +1,25 @@
+let NASDAQ = 0
+let SnP = 0
 game.splash("Welcome to the Stock Game Investor! ")
 pause(500)
 game.splash("What would you like to invest in?")
 pause(500)
-game.askForString("Here are your options! NASDAQ and S&P", 6)
-if ("S&P") {
+game.askForString("Here are your options! NASDAQ and SnP", 6)
+//  if S&P true then splash their choice then ask how much they want to invest and their current wallet amount
+if ("SnP") {
     true
+    SnP = randint(10000, 1000000)
 }
 
 game.splash("you've chosen the S&P!")
 pause(500)
-game.splash("This is its current Value: ")
+game.splash("This is its current Value: " + "$" + ("" + SnP))
+//  same stuff just for Nas
+if ("NASDAQ") {
+    true
+    NASDAQ = randint(10000, 1000000)
+}
+
+game.splash("you've chosen NASDAQ")
+pause(500)
+game.splash("This is its current Value: " + "$" + ("" + NASDAQ))
